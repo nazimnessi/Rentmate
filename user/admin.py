404594af6a4dfee0444d2d1310d27da67ee30255
nazim.vk <nazim.vk@scrapehero.com>
2023-04-01 16:49:51 +0530

    removed redunted code

--- user/admin.py
+++ user/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from .models import Documents, Address, User
-# Register your models here.
-from .models import *
 
 # Register your models here.
 admin.site.register(Documents)
