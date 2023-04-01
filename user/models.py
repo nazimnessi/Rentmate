b0ab180e9527121a27a1506ecf4ef029ac3053c9
nazim.vk <nazim.vk@scrapehero.com>
2023-04-01 16:52:36 +0530

    removed unwanted code

--- user/models.py
+++ user/models.py
@@ -1,6 +1,6 @@
 import phonenumbers
 from django.db import models
-from django.core.validators import FileExtensionValidator, RegexValidator
+from django.core.validators import FileExtensionValidator
 from django.core.exceptions import ValidationError
 
 # Create your models here.
