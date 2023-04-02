12beb725334f6b7b8b4b61f0af34d68efbd878d2
nazim.vk <nazim.vk@scrapehero.com>
2023-04-02 17:11:39 +0530

    admin page filter added

--- user/admin.py
+++ user/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from .models import Documents, Address, User
 
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone_number')
+    list_filter = ('name', 'email')
+
+    def api_name(self, obj):
+        return obj.user.name
+
+
 # Register your models here.
 admin.site.register(Documents)
 admin.site.register(Address)
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
