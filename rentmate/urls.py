582fa107c0d037547f6b81294522174911c61a3f
nazim.vk <nazim.vk@scrapehero.com>
2023-04-02 17:06:31 +0530

    graphql path added

--- rentmate/urls.py
+++ rentmate/urls.py
@@ -17,7 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
+from graphene_django.views import GraphQLView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
