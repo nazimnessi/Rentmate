02356561ec4ea5416efee709751deca096023b1f
nazim.vk <nazim.vk@scrapehero.com>
2023-04-07 20:06:37 +0530

    invluded bulidings urls.py to rentmate urls.py

--- rentmate/urls.py
+++ rentmate/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True))),
     path('api/', include('user.urls')),
+    path('api/', include('building.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
