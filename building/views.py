4004ccbb8386684faae11f962b02c290af84c8cb
nazim.vk <nazim.vk@scrapehero.com>
2023-04-10 23:14:50 +0530

    added flake8

--- building/views.py
+++ building/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, permissions
+from rest_framework import generics
 from rest_framework.parsers import FileUploadParser, MultiPartParser, FormParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
--- rentmate/urls.py
+++ rentmate/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
