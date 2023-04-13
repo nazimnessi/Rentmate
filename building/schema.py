05134dd24f3bf00790ce43fd7c0b1e2d7551c57e
nazim.vk <nazim.vk@scrapehero.com>
2023-04-14 00:03:56 +0530

    new schema added

--- building/schema.py
+++ building/schema.py
@@ -7,15 +7,32 @@ from .models import Building, Room, Request
 from .tasks import reject_requests
 
 
+class ExtendedConnection(graphene.Connection):
+    class Meta:
+        abstract = True
+
+    total_count = graphene.Int()
+    edge_count = graphene.Int()
+
+    def resolve_total_count(root, info, **kwargs):
+        return root.length
+
+    def resolve_edge_count(root, info, **kwargs):
+        return len(root.edges)
+
+
 class BuildingType(DjangoObjectType):
+
     class Meta:
         model = Building
         filter_fields = {
             'name': ['exact', 'icontains', 'istartswith'],
             'house_number': ['exact', 'icontains', 'istartswith'],
+            'owner': ['exact'],
         }
         interfaces = (relay.Node,)
         fields = '__all__'
+        connection_class = ExtendedConnection
 
 
 class RoomType(DjangoObjectType):
