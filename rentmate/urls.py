3ae66d2ee0e3677159995fc94b444dbed7e7f983
nazim.vk <nazim.vk@scrapehero.com>
2023-04-15 16:42:19 +0530

    allauth  url pattern added

--- rentmate/urls.py
+++ rentmate/urls.py
@@ -19,10 +19,12 @@ from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True))),
     path('api/', include('user.urls')),
     path('api/', include('building.urls')),
+    path('accounts/', include('allauth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
