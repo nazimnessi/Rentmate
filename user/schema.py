edd69a3411c4bdd7b769f847976bcf60a81a1c95
nazim.vk <nazim.vk@scrapehero.com>
2023-04-20 03:25:06 +0530

    schema added

--- user/schema.py
+++ user/schema.py
@@ -4,9 +4,39 @@ from graphene_file_upload.scalars import Upload
 from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
 from .models import User, Address
+from building.models import Building, Room
+from building.schema import RoomType
+from django.db.models import Count
+
+
+class RenterType(DjangoObjectType):
+
+    class Meta:
+        model = User
+        filter_fields = {
+            "email": ['exact'],
+            'username': ['exact', 'icontains', 'istartswith'],
+            'phone_number': ['exact', 'icontains', 'istartswith'],
+            'alt_phone_number': ['exact', 'icontains', 'istartswith'],
+        }
+        interfaces = (relay.Node,)
+        fields = '__all__'
 
 
 class UserType(DjangoObjectType):
+    total_buildings = graphene.Int()
+    total_renters = DjangoFilterConnectionField(RenterType)
+    total_rooms = DjangoFilterConnectionField(RoomType)
+
+    def resolve_total_rooms(parent, info, **kwargs):
+        return Room.objects.filter(building__owner=parent)
+
+    def resolve_total_renters(parent, info, **kwargs):
+        return User.objects.filter(room__building__owner=parent)
+
+    def resolve_total_buildings(parent, info, **kwargs):
+        return Building.objects.filter(owner=parent).aggregate(Count('id'))['id__count']
+
     class Meta:
         model = User
         filter_fields = {
