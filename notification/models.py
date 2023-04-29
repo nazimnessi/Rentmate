92b042b36b9373f91a073fb08a4815e950be61c7
nazim.vk <nazim.vk@scrapehero.com>
2023-04-29 17:42:38 +0530

    models updated and unwanted migration files delete

--- notification/models.py
+++ notification/models.py
@@ -5,17 +5,17 @@ from user.models import User
 
 class Notifications(models.Model):
     NOTIFICATION_TYPES = (
-        ('payment', 'Payment'),
-        ('maintenance', 'Maintenance'),
-        ('application', 'Application'),
-        ('others', 'Others'),
+        ('Payment', 'Payment'),
+        ('Maintenance', 'Maintenance'),
+        ('Application', 'Application'),
+        ('Others', 'Others'),
     )
     recipient = models.ForeignKey(User, on_delete=models.CASCADE)
     created_date = models.DateTimeField('Notifications created date', auto_now_add=True)
     updated_date = models.DateTimeField('Notifications updated date', auto_now_add=True)
     message = models.CharField(max_length=255, blank=True, null=True)
     is_read = models.BooleanField(default=False)
-    notification_type = models.CharField(max_length=20, choices=NOTIFICATION_TYPES, default='others')
+    notification_type = models.CharField(max_length=20, choices=NOTIFICATION_TYPES, default='Others')
     description = models.TextField(blank=True, null=True)
 
     def __str__(self):
