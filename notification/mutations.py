01af46a6dd9bab5569086f3bb1c23c54ebf1329e
nazim.vk <nazim.vk@scrapehero.com>
2023-05-27 00:03:46 +0530

    notification completd

--- notification/mutations.py
+++ notification/mutations.py
@@ -1,5 +1,5 @@
 import graphene
-from notification.node import NotificationsInput, NotificationsType
+from notification.node import NotificationsInput, NotificationsReadInput, NotificationsType
 from .models import Notifications
 
 
@@ -20,16 +20,28 @@ class CreateNotifications(graphene.Mutation):
 
 class UpdateNotifications(graphene.Mutation):
     class Arguments:
-        notification_data = NotificationsInput(required=True)
+        data = NotificationsInput(required=True)
     notifications = graphene.Field(NotificationsType)
 
     @staticmethod
-    def mutate(root, info, notification_data=None):
-        Notifications.objects.update_or_create(pk=notification_data.id, defaults=notification_data)
-        notification_instance = Notifications.objects.get(pk=notification_data.id)
+    def mutate(root, info, data=None):
+        Notifications.objects.update_or_create(pk=data.id, defaults=data)
+        notification_instance = Notifications.objects.get(pk=data.id)
         return UpdateNotifications(notifications=notification_instance)
+
+
+class NotificationRead(graphene.Mutation):
+    class Arguments:
+        data = NotificationsReadInput(required=True)
+    message = graphene.String()
+
+    @staticmethod
+    def mutate(root, info, data=None):
+        Notifications.objects.filter(id__in=data.get('ids')).update(is_read=True)
+        return NotificationRead(message="All Notifications read")
 
 
 class Mutation(graphene.ObjectType):
     create_notification = CreateNotifications.Field()
     update_notification = UpdateNotifications.Field()
+    is_notification_read = NotificationRead.Field()
