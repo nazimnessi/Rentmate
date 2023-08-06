1bf3db06f85ff98c264a11d1d0cd355733eccca7
nazimvk <nazimvk@gmail.com>
2023-08-06 14:23:12 +0530

    export added for property table

--- building/utils.py
+++ building/utils.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
+import csv
+
+
 def get_or_404(model, **kwargs):
     try:
         return model.objects.get(**kwargs)
@@ -10,3 +14,17 @@ def convert_string_to_display(string):
     capitalized_words = [word.capitalize() for word in words]
     display_string = ' '.join(capitalized_words)
     return display_string
+
+
+def data_export(data_list, column_names, datasetValues, filename, *args, **kwargs):
+    response = HttpResponse(content_type="text/csv")
+    response["Content-Disposition"] = f"attachment; filename={filename}"
+    writer = csv.DictWriter(response, column_names)
+    writer.writeheader()
+    for data in data_list:
+        data = data.__dict__
+        data_row = {}
+        for column in column_names:
+            data_row[column] = data[datasetValues[column]]
+        writer.writerow(data_row)
+    return response
