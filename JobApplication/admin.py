fe6ef1093efb7777373ad2b86115fd97939f5475
nazimvk <nazimvk@gmail.com>
2023-09-01 00:18:34 +0530

    admin added

--- JobApplication/admin.py
+++ JobApplication/admin.py
@@ -3,5 +3,17 @@ from JobApplication.models import Job_List, Job_Application
 
 # Register your models here.
 
-admin.site.register(Job_List)
-admin.site.register(Job_Application)
+
+class Job_ListAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'job_type')
+    search_fields = ['id', 'name', 'job_type']
+    list_filter = ('job_type',)
+
+
+class Job_ApplicationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'full_name', 'Job_List', 'email', 'total_experience', 'phone_number')
+    search_fields = ('id', 'full_name', 'Job_List', 'email', 'total_experience', 'phone_number')
+
+
+admin.site.register(Job_List, Job_ListAdmin)
+admin.site.register(Job_Application, Job_ApplicationAdmin)
