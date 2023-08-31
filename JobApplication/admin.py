a2d2ccfa27e361c0173d3e76b0f3b2100e14bc25
nazimvk <nazimvk@gmail.com>
2023-08-31 22:59:00 +0530

    new application added

---
+++ JobApplication/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from JobApplication.models import Job_List, Job_Application
+
+# Register your models here.
+
+admin.site.register(Job_List)
+admin.site.register(Job_Application)
