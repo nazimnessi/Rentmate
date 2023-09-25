4849efc6e67b7cad28b34368a04a990f7a825222
jobin joseph <jobin.joseph@scrapehero.com>
2023-09-25 23:03:19 +0530

    flake8 fix

--- payment/models.py
+++ payment/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from user.models import User
 from building.models import Room
 
+
 class Payment(models.Model):
     PAYMENT_STATUS_CHOICES = (
         ('Pending', 'Pending'),
